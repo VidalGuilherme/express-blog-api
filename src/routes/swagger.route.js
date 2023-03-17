975e75af5a62fba18db101dff11f6968e0aff8c2
Guilherme Vidal <guileads@gmail.com>
2023-03-17 09:00:35 -0300

    swagger

---
+++ src/routes/swagger.route.js
@@ -0,0 +1,17 @@
+import swaggerUi from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
+import swaggerDocument from "../swagger.json" assert {type: "json"};
+import {Router} from 'express';
+const router = Router();
+
+const swaggerOptions = {
+    swaggerDefinition: swaggerDocument,
+    apis: ['./src/routes/*.js']
+}
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+router.use("/", swaggerUi.serve);
+router.get("/", swaggerUi.setup(swaggerDocs));
+
+export default router;
--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -5,7 +5,30 @@ import {validAuth} from '../middlewares/auth.middlewares.js';
 
 const router = express.Router();
 
+// Routes
+/**
+ * @swagger
+ * /users:
+ *  get:
+ *      summary: Get all Users,
+ *      description: Use to requesta all users
+ *      tags: ["Users"]
+ *      responses:
+ *          '200':
+ *              description: Asucessful response 
+ */
 router.get('/', validAuth, (req,res) => userController.list(req, res));
+/**
+ * @swagger
+ * /users:
+ *  put:
+ *      summary: Find a User,
+ *      description: Use to update a user
+ *      tags: ["Users"]
+ *      responses:
+ *          '201':
+ *              description: Asucessful response 
+ */
 router.get('/:id', validAuth, validId, validUser, (req,res) => userController.find(req, res));
 router.post('/', (req, res) => userController.create(req, res));
 router.patch('/:id', validAuth, validId, validUser, (req, res) => userController.update(req, res));
