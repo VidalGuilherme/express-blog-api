2f3f724b1f2c285f61de562494dd202cb8553896
Guilherme <guileads@gmail.com>
2023-03-30 14:47:51 -0300

    fix dot env auth service

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,4 +1,6 @@
 import jwt from "jsonwebtoken";
+import dotenv from 'dotenv';
+dotenv.config(); 
 
 const generateToken = (id) => jwt.sign({id:id}, process.env.JWT_SECRET, {expiresIn:process.env.JWT_EXPIRES});
 const generateTokenAdmin = (id) => jwt.sign({id:id}, process.env.JWT_ADMIN_SECRET, {expiresIn:process.env.JWT_ADMIN_EXPIRES});
